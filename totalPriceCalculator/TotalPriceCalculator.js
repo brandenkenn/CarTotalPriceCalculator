b9d9564f14d1574576047224423ffe2c2c918faf
Branden <brandenkenn@gmail.com>
2019-07-11 19:33:16 -0400

    added trade info, down payment, and a monthly payment calc

--- totalPriceCalculator/TotalPriceCalculator.js
+++ totalPriceCalculator/TotalPriceCalculator.js
@@ -5,23 +5,13 @@ function totalPrice () {
     let registration = parseFloat(document.getElementById("registration").value)
     let rebates = parseInt(document.getElementById("rebates").value)
     let misc = parseInt(document.getElementById("misc").value)
-    let result = (((basePrice + docFee - rebates + misc) * (1 + (taxRate/100))) + registration).toFixed(2)
-    console.log(`
-        Base Price: $${basePrice}
-        Doc Fee: $${docFee}
-        Tax Rate: ${taxRate}%
-        Registration: $${registration.toFixed(2)}
-        Rebates: $${rebates}
-        Miscellaneous: $${misc}
-        Total Price: $${result} `)
+    let payoff = parseInt(document.getElementById("payoff").value)
+    let tradeValue = parseInt(document.getElementById("tradeValue").value)
+    let downPayment = parseInt(document.getElementById("downPayment").value)
+    let result = (((basePrice + docFee + misc - tradeValue + payoff) * (1 + (taxRate/100))) - rebates - downPayment + registration).toFixed(2)
+    
     document.getElementById("result").innerHTML = `<br>
-        Base Price: $${basePrice}<br>
-        Doc Fee: $${docFee}<br>
-        Tax Rate: ${taxRate}%<br>
-        Registration: $${registration.toFixed(2)}<br>
-        Rebates: $${rebates}<br>
-        Miscellaneous: $${misc}<br>
-        Total Price: $${result}`
+        Total Price: $${result}`    
 }
 
 const input = document.getElementById("input");
@@ -32,5 +22,21 @@ input.addEventListener("keyup", function(event) {
     document.getElementById("myBtn").click();
   }
 });
+
+function monthlyPaymentCalc () {
+  let principal = parseFloat(document.getElementById("principal").value)
+  let interestRate = parseFloat(document.getElementById("interestRate").value)
+  let monthlyInterest = (interestRate/100)/12
+  let term = parseInt(document.getElementById("term").value)
+  let x = Math.pow(1 + monthlyInterest, term)
+  let monthlyPayment = ((principal * x * monthlyInterest)/(x - 1)).toFixed(2)
+
+// compute the monthly payment figure
+// var x = Math.pow(1 + interest, payments); //Math.pow computes powers
+// var monthly = (principal*x*interest)/(x-1);
+
+  document.getElementById("monthlyPayment").innerHTML = `<br>
+        Monthly Payment: $${monthlyPayment}`
+}
 
 
